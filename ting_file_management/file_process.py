afdd84214e6290efb04d662b2768f971f6a69503
vcscunha <vcs.cunha@icloud.com>
2023-09-29 14:35:57 -0300

    req 03: implementa a funcao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file: str, instance: Queue):
+    for arquivo in instance.queue:
+        if arquivo["nome_do_arquivo"] == path_file:
+            return None
+
+    linhas_arquivo = txt_importer(path_file)
+    arquivo_processado = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(linhas_arquivo),
+        "linhas_do_arquivo": linhas_arquivo,
+    }
+    instance.enqueue(arquivo_processado)
+    print(arquivo_processado)
 
 
 def remove(instance):
