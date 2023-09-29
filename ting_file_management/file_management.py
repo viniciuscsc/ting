cff1f9374eb6537127794641ebb8262af23bad9d
vcscunha <vcs.cunha@icloud.com>
2023-09-29 12:43:31 -0300

    req 02: implementa a funcao txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,9 @@
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, "r") as arquivo_noticias:
+            linhas_arquivo = arquivo_noticias.read().split("\n")
+            return linhas_arquivo
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado")
+        return []
