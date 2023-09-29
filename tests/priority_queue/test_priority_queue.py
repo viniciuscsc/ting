abc732160039bf0a363cea85a6a33b10d1a4d855
vcscunha <vcs.cunha@icloud.com>
2023-09-29 15:31:36 -0300

    req 06: implementa testes para a funcao enqueue da classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,29 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    arquivo_regular = {
+        "nome_do_arquivo": "arquivo_regular.txt",
+        "qtd_linhas": 6,
+        "linhas_do_arquivo": ["L1", "L2", "L3", "L4", "L5", "L6"],
+    }
+
+    arquivo_prioritario = {
+        "nome_do_arquivo": "arquivo_prioritario.txt",
+        "qtd_linhas": 4,
+        "linhas_do_arquivo": ["L1", "L2", "L3", "L4"],
+    }
+
+    fila_prioridade = PriorityQueue()
+
+    # listas regular_priority e high_priority estão vazias
+    assert len(fila_prioridade.regular_priority) == 0
+    assert len(fila_prioridade.high_priority) == 0
+
+    # arquivo_regular é inserido em regular_priority
+    fila_prioridade.enqueue(arquivo_regular)
+    assert len(fila_prioridade.regular_priority) == 1
+
+    # arquivo_prioritario é inserido em high_priority
+    fila_prioridade.enqueue(arquivo_prioritario)
+    assert len(fila_prioridade.high_priority) == 1
